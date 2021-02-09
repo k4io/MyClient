5bbfde9c807c11bca55b81ad955e047b705143a7
kai <atlantixdreamable@gmail.com>
2021-02-09 21:19:29 +0000

    Add project files.

---
+++ MyClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+public class Whois
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 1) return;
+        int c;
+        TcpClient client = new TcpClient();
+        client.Connect("whois.networksolutions.com", 43);
+        StreamWriter sw = new StreamWriter(client.GetStream());
+        StreamReader sr = new StreamReader(client.GetStream());
+        sw.WriteLine(args[0]);
+        sw.Flush();
+        Console.WriteLine(sr.ReadToEnd());
+    }
+}
